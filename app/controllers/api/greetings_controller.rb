ce47a98c2bbe31f0947a740ce43907c872c5a4ed
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-12 17:36:48 +0300

    Create the route and corrected the errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,9 +1,7 @@
-module Api
-    class GreetingsController < ApplicationController
-      def random
+class Api::GreetingsController < ApplicationController
+    def random
         random_greeting = Message.order("RANDOM()").first
         render json: { greeting: random_greeting.greeting }
-      end
     end
-  end
+end
   
