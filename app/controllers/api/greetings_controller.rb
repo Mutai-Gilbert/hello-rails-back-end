21c63c63923ab4a4f7371bc54da13cf2d06ae37e
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-12 17:26:13 +0300

    Create an API endpoint for random greetings

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,9 @@
+module Api
+    class GreetingsController < ApplicationController
+      def random
+        random_greeting = Message.order("RANDOM()").first
+        render json: { greeting: random_greeting.greeting }
+      end
+    end
+  end
+  
